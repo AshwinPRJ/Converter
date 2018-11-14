f3c09c35c4e801abf538d8084fbea24a3a262705
AshwinPRJ <ashwin.prj@gmail.com>
2018-11-14 11:58:35 +0530

    controller create for test

---
+++ Converter-master/src/main/java/com/ashwin/converter/HomeController.java
@@ -0,0 +1,12 @@
+package com.ashwin.converter;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@RequestMapping(value = "/home")
+	public String check() {
+		return "Hello World!!!";
+	}
+}
