ebc43dd6ae7282b3f350b82bc7fe50a486a4d5b6
AshwinPRJ <ashwin.prj@gmail.com>
2018-11-14 12:34:59 +0530

    logger is added

--- Converter-master/src/main/java/com/ashwin/converter/HomeController.java
+++ Converter-master/src/main/java/com/ashwin/converter/HomeController.java
@@ -1,12 +1,19 @@
 package com.ashwin.converter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 	@RequestMapping(value = "/home")
 	public String check() {
+		LOGGER.debug("This is a debug message");
+	    LOGGER.info("This is an info message");
+	    LOGGER.warn("This is a warn message");
+	    LOGGER.error("This is an error message");
 		return "Hello World!!!";
 	}
 }
